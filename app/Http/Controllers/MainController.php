8fd21a9212226fa270b9e28c4e115b3914de2d95
Akhram <akhramshaik@gmail.com>
2018-07-01 17:48:29 +0530

    First Setup of Form

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -28,8 +28,23 @@ class MainController extends Controller
 
 public function wesupport(){
 
-	// $location = GeoIP::getLocation( );
- //       return  $locationInfo  = [ 'userIp' => $location['ip'], 'userCountry' => $location['country'], 'userCity' => $location['city'], 'userState' => $location['state_name'], 'userPincode' => $location['postal_code'] ];
+
+	$location = GeoIP::getLocation( );
+    date_default_timezone_set("Asia/Kolkata");
+
+	$arrayData = ['userIp' => $location['ip'],
+	              'userCity' => $location['city'],
+	              'userPincode' => $location['postal_code'],
+	              'userState' => $location['state_name'],
+	              'userCountry' => $location['country'],
+	              'userTimestamp' => date("Y-M-d h:i:s A")
+	          ];
+
+	    $createVisit = Visit::create($arrayData);
+
+
+
+
        
 
 
@@ -53,6 +68,46 @@ public function television(){
 	return view('support.media');
 }
 
+
+public function voteprocess(Request $request){
+	$location = GeoIP::getLocation( );
+    date_default_timezone_set("Asia/Kolkata");
+
+
+	$arrayData = ['userName' => $request['fullname'],
+	              'userOccupation' => $request['occupation'],
+	              'userEmail' => $request['emailid'],
+	              'userMobile' => $request['phone'],
+	              'userMsg' => $request['message'],
+	              'userIp' => $location['ip'],
+	              'userCity' => $location['city'],
+	              'userPincode' => $location['postal_code'],
+	              'userState' => $location['state_name'],
+	              'userCountry' => $location['country'],
+	              'userTimestamp' => date("Y-M-d h:i:s A")
+	          ];
+
+
+	    $findUser = Support::where('userEmail',$request['emailid'])->orWhere('userMobile',$request['phone'])->count();
+
+        if($findUser >= 1){
+            Session::flash('user_voted', 'Thanks for your support.'); 
+	        return redirect(route('vote'));  
+        } 
+
+
+	    $createVote = Support::create($arrayData);
+        if($createVote){
+            Session::flash('msg_ok', 'Thanks for your support.'); 
+        } else{
+            Session::flash('chk_msg', 'Warning! Something went wrong. Try again later.'); 
+        }
+
+        return redirect(route('vote'));  
+
+
+
+}
 
 
 // vote
--- resources/views/support/vote.blade.php
+++ resources/views/support/vote.blade.php
@@ -8,11 +8,34 @@
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 pull-right ">
                <div id="tg-content" class="tg-content">
                   <div class="tg-donatenow">
+
+@if(Session::has('msg_ok'))
+<div class="alert alert-success">
+Thank you for support. Help us in sharing this and lets make this campain successful together. 
+</div>
+@endif
+
+
+@if(Session::has('user_voted'))
+<div class="alert alert-danger">
+This email or Mobile number als  make this campain successful together. 
+</div>
+@endif
+
+
+@if(Session::has('chk_msg'))
+<div class="alert alert-danger">
+Sorry... Something went wrong. Please try again later.
+</div>
+@endif
+
+
                      <div class="tg-sectiontitle">
                         <h2>Support now</h2>
                      </div>
                      <form class="tg-themeform tg-formdonation" action="{{ route('voteprocess') }}" method="POST" autocomplete="off" enctype="multipart/form-data">
                         <fieldset>
+
                            <div class="tg-donationstep">
                               <div class="tg-steptitle">
                                  <p>This is just to show my own interest to support bg...a </p>
@@ -34,6 +57,8 @@
                                     <textarea name="message" class="form-control" placeholder="Message ( Optional )"></textarea>
                                  </div>
                               </div>
+                              <input type="hidden" id="csrf-token" value="{{csrf_token()}}" name="_token">
+
                               <button class="tg-btn" type="submit">Submit</button>
                            </div>
                            <p style="color: #e87e7e;font-size: 10px;"> * Information collected here is completely for non commertial use and no where realted to babu gogineni...this information is seccured and safe using encrypted algorithms under high secured environmnet</p>
